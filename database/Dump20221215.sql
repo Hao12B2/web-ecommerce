1b3fa5f8a3fe3bb01fcb216164459cd29844b632
Hao12B2 <haovan01652500861@gmail.com>
2023-11-23 17:36:39 +0700

    IT Project

---
+++ database/Dump20221215.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Connection02
+ Source Server Type    : MySQL
+ Source Server Version : 80031
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 80031
+ File Encoding         : 65001
+
+ Date: 22/11/2023 16:07:39
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
